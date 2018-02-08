f0f477470839ff6788d7e80b2d457f364abf7fe5
pavelnbc <giperpasha@rambler.ru>
2018-02-09 00:16:33 +0200

    Updates for 08.02

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const DOWNLOAD_WEATHER_DATA = 'DOWNLOAD_WEATHER_DATA';
 
 export function downloadWeatherData(cityName) {
     return (dispatch) => {
-        return axios.post(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=873c01c831082a9acedd262b863a707d&units=metric`)
+        return axios.post(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=873c01c831082a9acedd262b863a707d&units=metric`)
             .then(response => response.data)
             .then(data => dispatch({
                 type: DOWNLOAD_WEATHER_DATA,
