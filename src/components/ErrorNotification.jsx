ad1feb24fa384d83c3d16d05c2824c4bd0ddcfaa
pavelnbc <giperpasha@rambler.ru>
2018-02-10 15:34:06 +0200

    Updates for 10.02

--- src/components/ErrorNotification.jsx
+++ src/components/ErrorNotification.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import classNames from 'classnames';
 
 function ErrorNotification({ isError, closeNotification }) {
@@ -9,12 +10,18 @@ function ErrorNotification({ isError, closeNotification }) {
     });
 
     return (
-        <div className={errorNotificationClass} onClick={closeNotification}>
+        <div className={errorNotificationClass}>
             <img src="/img/not-found.png" alt="not found"/>
+
             <h1>Nothing was found</h1>
+
             <p>We are extremely sorry, but we couldn't find anything.</p>
+
             <p>Possibly that happened because you've made a mistake typing your city name.</p>
+
             <p>Please, try to repeat the procedure once again.</p>
+
+            <Button bsStyle="primary" onClick={closeNotification}>Understood</Button>
         </div>
     )
 }
