e501b3d1da7283d2dd409fc1bffa9f6a11e55603
pavelnbc <giperpasha@rambler.ru>
2018-02-11 19:47:41 +0200

    Updates for 11.02

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
+import './css/index.css';
 import "font-awesome/css/font-awesome.css";
 
 import App from './App';
