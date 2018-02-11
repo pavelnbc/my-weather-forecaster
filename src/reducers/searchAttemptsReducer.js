9c97e3725ebe61b3a2fc567b2dcec66dba2e29ab
pavelnbc <giperpasha@rambler.ru>
2018-02-11 10:11:52 +0200

    Updates for 11.02

--- src/reducers/searchAttemptsReducer.js
+++ src/reducers/searchAttemptsReducer.js
@@ -4,15 +4,15 @@ function searchAttemptsReducer(state = [], action) {
     switch(action.type) {
         case ADD_CITY:
             for(let i = 0; i < state.length; i++) {
-                if(state[i] === action.cityName) return state
+                if(state[i] === action.cityName) return state;
             }
 
-            let cityList = [action.cityName, ...state, ];
-            cityList.length = cityList.length <= 10 ? cityList.length : 10
+            let cityList = [action.cityName, ...state ];
+            cityList.length = cityList.length <= 10 ? cityList.length : 10;
             return cityList;
 
         default:
-            return state
+            return state;
     }
 }
 
