9cb32a49a7db2816391d2cf934f37d64fef26a9a
pavelnbc <giperpasha@rambler.ru>
2018-02-15 16:02:05 +0200

    Updates for 15.02

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -29,10 +29,12 @@ class LeftSideBar extends Component {
     }
 
     selectLastSearch(event) {
-        this.props.downloadWeather(event.target.closest('li').textContent);
+        let attempt = event.target.closest('button').textContent;
+
+        this.props.downloadWeather(attempt);
 
         this.setState({
-            searchValue: event.target.closest('li').textContent
+            searchValue: attempt
         })
     }
 
@@ -51,7 +53,6 @@ class LeftSideBar extends Component {
                     </ControlLabel>
                     <FormControl
                         type="text"
-                        id="input"
                         value={this.state.searchValue}
                         placeholder="London, Paris, Rome..."
                         onChange={this.handleChange}
@@ -61,10 +62,10 @@ class LeftSideBar extends Component {
 
                 <section className="last-search">
                     <ControlLabel>Last time you searched...</ControlLabel>
-                    <ListGroup onClick={this.selectLastSearch}>
+                    <ListGroup >
                         { searchAttempts.length
                           ? searchAttempts.map((city, index) => {
-                                return <ListGroupItem key={index}>
+                                return <ListGroupItem key={index} onClick={this.selectLastSearch}>
                                             <FontAwesome name="map-marker"/>
                                             <span>{city}</span>
                                        </ListGroupItem>
@@ -78,15 +79,92 @@ class LeftSideBar extends Component {
     }
 }
 
-/*<ListGroupItem>London</ListGroupItem>
-                        <ListGroupItem>Moscow</ListGroupItem>
-                        <ListGroupItem>Paris</ListGroupItem>
-                        <ListGroupItem>Budapest</ListGroupItem>
-                        <ListGroupItem>Madrid</ListGroupItem>*/
-
-
 LeftSideBar.propTypes = {
+   searchAttempts: PropTypes.array,
    downloadWeather: PropTypes.func
 };
 
 export default LeftSideBar
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
+import { FormControl, ListGroup, ListGroupItem } from 'react-bootstrap';
+
+class LeftSideBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchValue: "Kiev"
+        };
+
+        this.handleInputValue = this.handleInputValue.bind(this);
+        this.handleSend = this.handleSend.bind(this);
+        this.handleAttempt = this.handleAttempt.bind(this);
+    }
+
+    handleInputValue(event) {
+        this.setState({
+            searchValue: event.target.value
+        })
+    }
+
+    handleSend(event) {
+        event.preventDefault();
+
+        this.props.downloadWeather(this.state.searchValue);
+    }
+
+    handleAttempt(event) {
+        let attempt = event.target.closest('button').textContent;
+
+        this.props.downloadWeather(attempt);
+
+        this.setState({
+            searchValue: attempt
+        })
+    }
+
+    componentDidMount() {
+        this.props.downloadWeather(this.state.searchValue);
+    }
+
+    render() {
+        let { searchAttempts } = this.props;
+
+        return(
+            <aside className="left-side-bar">
+                <form onSubmit={this.handleSend}>
+                    <FormControl type="text"
+                                 placeholder="London, Paris, Rome"
+                                 value={this.state.searchValue}
+                                 onChange={this.handleInputValue}
+                    />
+                    <FontAwesome name="search" onClick={this.handleSend}/>
+                </form>
+                <ListGroup>
+                    {searchAttempts.map((attempt, index) => {
+                        return (
+                            <ListGroupItem key={index} onClick={this.handleAttempt}>
+                                <FontAwesome name="map-marker"/>
+                                {attempt}
+                            </ListGroupItem>
+                        )
+                    })}
+                </ListGroup>
+            </aside>
+        )
+    }
+}*/
