f498a6f163c32efff1187c52989f7c646b9ee787
pavelnbc <giperpasha@rambler.ru>
2018-02-20 12:02:18 +0200

    Updates for 20.02

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function DailyForecast({ item, forecastDetails }) {
     let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    let date = new Date(forecastDetails.list[item].dt * 1000).getDay();
+    let date = new Date(forecastDetails.list[item].dt).getDay();
     let minTemp = Math.floor(forecastDetails.list[item].main.temp_min) <= 0
             ? Math.floor(forecastDetails.list[item].main.temp_min)
             : `+${Math.floor(forecastDetails.list[item].main.temp_min)}`;
@@ -17,14 +17,12 @@ function DailyForecast({ item, forecastDetails }) {
     return (
         <li className="forecast">
             <h3>{week[date]}</h3>
-            <div>
-                <span>{minTemp}</span>
-                <span>...</span>
-                <span>{maxTemp}</span>
-                <span>
-                    <img src={iconSrc} alt="weatherIcon"/>
-                </span>
-            </div>
+            <span>{minTemp}</span>
+            <span>...</span>
+            <span>{maxTemp}</span>
+            <span>
+                <img src={iconSrc} alt="weatherIcon"/>
+            </span>
         </li>
     )
 }
